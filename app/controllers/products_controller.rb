4c0d3a971a7b6cbe100dd1a5b19e3a76c5c5e50b
noel rodriguez <noelrodriguez@noels-MacBook.fios-router.home>
2021-01-30 17:56:59 -0500

    back up

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+class ProductsController < ApplicationController
+
+
+# INDEX METHOD
+    def index
+        render  json: Product.all
+    end
+# SHOW METHOD
+    def show
+       render json: Product.find(params['id'])
+    end
+# CREATE METHOD
+    def create
+        render json: Product.create(params['product'])
+
+    end
+# DELETE METHOD
+    def delete
+        render json: Product.delete(params['id'])
+    end
+# UPDATE METHOD
+   def update
+       render json: Product.update(params['id'], params['product'])
+
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# INDEX ROUTE
+ get '/menwear', to: 'products#index'
+ get '/womenwear', to: 'products#index'
+ get '/thriftwear', to: 'products#index'
+# SHOW ROUTE
+get 'menwear/:id', to: 'products#show'
+#CREATE ROUTE
+post '/menwear', to: 'products#create'
+#DELETE ROUTE
+delete '/menwear/:id', to: 'products#delete'
+#UPDATE ROUTE
+put '/menwear/:id', to: 'products#update'
+
 end
