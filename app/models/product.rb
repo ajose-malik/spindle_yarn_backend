d2c3af338773e4a2577b4b49bfa88e09b451754c
noel rodriguez <noelrodriguez@noels-MacBook.fios-router.home>
2021-01-31 09:59:26 -0500

    bugs fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product
-    if ENV["DATABASE_URL"]
-  PG.connect(ENV['DATABASE_URL'])
-elsif
+    if(ENV['DATABASE_URL'])
+      uri = URI.parse(ENV['DATABASE_URL'])
+      DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+else
   DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'spindle_yarn_backend_development'})
 end
 # INDEX
