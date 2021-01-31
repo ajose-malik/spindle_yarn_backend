42bf2d9c633facb83c3f497dd2418f84f986caf2
ajose-malik <ajose.malik@gmail.com>
2021-01-30 21:16:23 -0500

    Heroku test

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 # INDEX ROUTE
- get '/womenwear', to: 'products#index'
+ get '/menwear', to: 'products#index'
 #  get '/womenwear', to: 'products#index'
 #  get '/thriftwear', to: 'products#index'
 # SHOW ROUTE
