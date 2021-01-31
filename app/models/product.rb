0a7e65d85f2b80e151e9a079fc98f18bad4caa2d
ajose-malik <ajose.malik@gmail.com>
2021-01-30 21:12:16 -0500

    Heroku test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,8 @@
 class Product
-    # if ENV["DATABASE_URL"]
+    if ENV["DATABASE_URL"]
   PG.connect(ENV['DATABASE_URL'])
-# uri = URI.parse(ENV['DATABASE_URL'])
-DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
-# elsif
-#   DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'contacts_development'})
+elsif
+  DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'contacts_development'})
 end
 # INDEX
 
